1b67f5c9af87df03e24a81b2527ba8984c682e77
陳栄超 <48024543+chenmiaochao@users.noreply.github.com>
2021-08-23 23:15:11 +0900

    first component

---
+++ src/app/components/scrollable-tab/scrollable-tab.component.ts
@@ -0,0 +1,99 @@
+import { Component, OnInit } from '@angular/core';
+
+interface TopMenu {
+  title: string;
+ link: string;
+}
+
+interface AddFunc{
+  (x: number, y: number): number
+}
+
+
+@Component({
+  selector: 'app-scrollable-tab',
+  templateUrl: './scrollable-tab.component.html',
+  styleUrls: ['./scrollable-tab.component.css']
+})
+export class ScrollableTabComponent implements OnInit {
+  title = 'myPinduoduo';
+  selectedIndex = -1;
+  menus: TopMenu[] = [
+    {
+      title: '热门',
+      link: ''
+    },
+    {
+      title: '男装',
+      link: ''
+    },
+    {
+      title: '百货',
+      link: ''
+    },
+    {
+      title: '运动',
+      link: ''
+    },
+    {
+      title: '手机',
+      link: ''
+    },
+    {
+      title: '家纺',
+      link: ''
+    },
+    {
+      title: '食品',
+      link: ''
+    },
+    {
+      title: '电器',
+      link: ''
+    },
+    {
+      title: '鞋包',
+      link: ''
+    },
+    {
+      title: '汽车',
+      link: ''
+    },
+    {
+      title: '水果',
+      link: ''
+    },
+    {
+      title: '电脑',
+      link: ''
+    },
+    {
+      title: '内衣',
+      link: ''
+    },
+    {
+      title: '家装',
+      link: ''
+    },
+    {
+      title: '母婴',
+      link: ''
+    },
+    {
+      title: '美妆',
+      link: ''
+    },
+    {
+      title: '家具',
+      link: ''
+    }
+  ];
+  handlerSelection(index: number) {
+    this.selectedIndex = index;
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
