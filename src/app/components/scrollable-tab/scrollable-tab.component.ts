31da23aa230becda855f2308a0bfc868959ca4e5
陳栄超 <48024543+chenmiaochao@users.noreply.github.com>
2021-08-23 23:40:52 +0900

    @input @output emit

--- src/app/components/scrollable-tab/scrollable-tab.component.ts
+++ src/app/components/scrollable-tab/scrollable-tab.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
 
-interface TopMenu {
+export interface TopMenu {
   title: string;
- link: string;
+  link: string;
 }
 
 interface AddFunc{
@@ -18,78 +19,12 @@ interface AddFunc{
 export class ScrollableTabComponent implements OnInit {
   title = 'myPinduoduo';
   selectedIndex = -1;
-  menus: TopMenu[] = [
-    {
-      title: '热门',
-      link: ''
-    },
-    {
-      title: '男装',
-      link: ''
-    },
-    {
-      title: '百货',
-      link: ''
-    },
-    {
-      title: '运动',
-      link: ''
-    },
-    {
-      title: '手机',
-      link: ''
-    },
-    {
-      title: '家纺',
-      link: ''
-    },
-    {
-      title: '食品',
-      link: ''
-    },
-    {
-      title: '电器',
-      link: ''
-    },
-    {
-      title: '鞋包',
-      link: ''
-    },
-    {
-      title: '汽车',
-      link: ''
-    },
-    {
-      title: '水果',
-      link: ''
-    },
-    {
-      title: '电脑',
-      link: ''
-    },
-    {
-      title: '内衣',
-      link: ''
-    },
-    {
-      title: '家装',
-      link: ''
-    },
-    {
-      title: '母婴',
-      link: ''
-    },
-    {
-      title: '美妆',
-      link: ''
-    },
-    {
-      title: '家具',
-      link: ''
-    }
-  ];
+  @Input() menus: TopMenu[] = []
+  @Output() tabSelected = new EventEmitter()
+
   handlerSelection(index: number) {
     this.selectedIndex = index;
+    this.tabSelected.emit(this.menus[this.selectedIndex])
   }
   constructor() { }
 
